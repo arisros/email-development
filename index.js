19728ecc7c8430732d745884517990e3352bbde3
Aris Jirat Kurniawan <arisjiratkurniawan@Ariss-MacBook-Pro.local>
2019-02-06 14:38:08 +0700

    inline done

--- index.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const inlineCss = require('gulp-inline-css');
-
-gulp.task('default', function () {
-  console.log('code', process.env)
-  return gulp.src('shelter/**/*.html')
-    .pipe(inlineCss())
-    .pipe(gulp.dest('ready-to-fly/'));
-});
